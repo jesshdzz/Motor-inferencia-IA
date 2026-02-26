934e9f91539b12150f194265eb868b568af589b2
jesshdzz <jesushernandezz.dev@gmail.com>
2026-02-26 13:07:14 -0600

    clase basica del motor de inferencia

--- MotorInferencia.java
+++ MotorInferencia.java
@@ -1,5 +1,19 @@
-public class MotorInferencia {
-    // TODO: Implementar los algoritmos de encadenamiento (hacia atras y hacia
-    // adelante)
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class MotorInferencia {
+    private List<Regla> reglas;
+    private List<String> hechos;
+    private Scanner scanner;
+
+    public MotorInferencia(List<Regla> reglas, List<String> hechos) {
+        this.reglas = reglas;
+        this.hechos = hechos;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public List<String> getHechos() {
+        return this.hechos;
+    }
 }
